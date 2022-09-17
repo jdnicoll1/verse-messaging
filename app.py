0c61947f7c4130eb96773849f9be50fd5f56603e
Jacob Nicoll <jdnicoll1@gmail.com>
2022-09-16 23:59:45 -0500

    sms reply test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from twilio.rest import Client
 import os
+from twilio.twiml.messaging_response import MessagingResponse
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
@@ -42,32 +43,18 @@ def send_verse():
 
 @app.route('/', methods=['GET', 'POST'])
 def sms_reply(): 
-    return "yo"
-    # verse_dict = {"2 Corinthians 5:21" : "For our sake he made him to be sin who knew no sin, so that in him we might become the righteousness of God", 
-    #             "1 Thessalonians 5:16-18" : "Rejoice always, pray without ceasing, give thanks in all circumstances; for this is the will of God in Christ Jesus for you.", 
-    #             "1 Peter 1:13" : "Therefore, preparing your minds for action, and being sober-minded, set your hope fully on the grace that will be brought to you at the revelation of Jesus Christ.", 
-    #             "Ezekiel 36:26" : "And I will give you a new heart, and a new spirit I will put within you. And I will remove the heart of stone from your flesh and give you a heart of flesh.",
-    #             "Proverbs 3:5-6" : "Trust in the LORD with all your heart, And lean not on your own understanding; In all your ways acknowledge Him, And He shall direct your paths.",
-    #             "Mark 10:45" : "For even the Son of Man did not come to be served, but to serve, and to give His life a ransom for many.",
-    #             "Romans 8:32" : "He who did not spare His own Son, but delivered Him up for us all, how shall He not with Him also freely give us all things?"
-    # }
 
-    # number = request.form['From']
-    # message_body = request.form['Body']
+    number = request.form['From']
+    message_body = request.form['Body']
 
-    # resp = MessagingResponse()
-    # verse_num = 1
-    # response_message = 'Hello {}, You said: {}'.format(number, list(verse_dict)[verse_num] + ": " + list(verse_dict.values())[verse_num])
-    # resp.message(response_message)
+    resp = MessagingResponse()
+    response_message = 'Hello {}, You said: {}'.format(number, message_body)
+    resp.message(response_message)
 
-    # return str(resp)
+    return str(resp)
 
     
 
-
-
-
-    
 if __name__ == "__main__":
     sched.start()
     app.run()
