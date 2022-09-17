5fc4c9e5476252263c8f399c64b073246d9bc6d8
Jacob Nicoll <jdnicoll1@gmail.com>
2022-09-16 21:09:48 -0500

    sending message test

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+import os
+from twilio.rest import Client
 
 app = Flask(__name__)
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Hi there',
+                              from_='+19793254225',
+                              to='+13092654472'
+                          )
+
+print(message.sid)
 
 @app.route("/")
 def hello_world():
