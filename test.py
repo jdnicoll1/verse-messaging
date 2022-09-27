13f10276fbece3f4c95896de33578538808f232a
Jacob Nicoll <jdnicoll1@gmail.com>
2022-09-27 11:19:39 -0500

    updating 4

--- test.py
+++
@@ -1,8 +0,0 @@
-with open('books.txt') as file:
-    contents = file.read()
-    search_word = input("enter a word you want to search in file: ")
-    if search_word in contents:
-        print ('word found')
-    else:
-        print ('word not found')
-
