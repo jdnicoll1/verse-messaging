8f8050050b0867e18341310a8f729310c37a4e2e
Jacob Nicoll <jdnicoll1@gmail.com>
2022-09-27 12:22:23 -0500

    menu updated

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
 #text menu functions 
 
 def print_menu():
-    return "The Message Menu\n\nSend a text with a number 1-6:\n1 - My Verses\n2 - Add random verse to My Verses\n3 - Add custom verse to My Verses\n4 -  Choose daily verse from My Verses\n\nText STOP to stop all messages"
+    return "The Message Menu\n\nSend a text with a number 1-4:\n1 - My Verses\n2 - Add random verse to My Verses\n3 - Add custom verse to My Verses\n4 - Choose daily verse from My Verses\n\nText STOP to stop all messages"
