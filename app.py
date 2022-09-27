3578d0387787a305d5c3a55a70e6a9abb3a4ffc2
Jacob Nicoll <jdnicoll1@gmail.com>
2022-09-27 11:37:34 -0500

    option 4 implemented

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, request
 from twilio.rest import Client
 import os
@@ -58,12 +59,21 @@ def sms_reply():
     #initial message when signing up
     verse_init = {"1 Thessalonians 5:16-18" : "Rejoice always, pray without ceasing, give thanks in all circumstances; for this is the will of God in Christ Jesus for you."}
     if(verse_collection.count_documents({"phone_number": number}) == 0): #user is not in system yet and we need to add their number
-        verse_collection.insert_one({'phone_number': number, 'name': message_body, 'daily_verse': verse_init, 'verses': verse_init, 'on_text_chain': True, 'adding_custom_verse': False}) #add user to the database
+        verse_collection.insert_one({'phone_number': number, 'name': message_body, 'daily_verse': verse_init, 'verses': verse_init, 'on_text_chain': True}) #add user to the database
         response_message = 'Welcome to The Message!\n\nPaul says in Philippians 4:8: "Finally, brothers and sisters, whatever is true, whatever is noble, whatever is right, whatever is pure, whatever is lovely, whatever is admirable — if anything is excellent or praiseworthy—think about such things."\n\nIt is for this reason that this app was created... to learn more about God\'s Word and help set our thoughts on it consistently.\n\nBy signing up for the service you will receive a message every three hours. The verse will change every week or you can choose one yourself by texting MENU , which also has other options as well. Enjoy!!'
         
 
     else: #user is already in system 
         #parse what the user sent
+        #check to see if incoming message is new verse - we will know this by whether or not an equals sign is present 
+        split_verse = message_body.split('=')
+        if(len(split_verse) == 2):
+            user_doc = verse_collection.find_one({"phone_number": number})
+            user_obj = user_doc["verses"] #get verses specific user has
+            user_obj[split_verse[0]] = split_verse[1]
+            verse_collection.update_one({"phone_number": number}, {"$set": {"verses": user_obj}})
+            response_message = "{} added to my verses".format(split_verse[0])
+
         if(message_body == "MENU"):
             response_message = print_menu()
             verse_collection.update_one({"phone_number": number}, {"$set": {"adding_custom_verse": False}})
@@ -95,8 +105,7 @@ def sms_reply():
                     response_message = "{} added to my verses".format(admin_verse_reference)
                     break
         elif(message_body == "4"):
-            response_message = "To add a custom verse, send a text with the verse reference and the verse content separated by an equals sign. For example:\nJohn 11:35 = Jesus Wept."
-            verse_collection.update_one({"phone_number": number}, {"$set": {"adding_custom_verse": True}})
+            response_message = "To add a custom verse, send a text with the verse reference and the verse content separated by an equals sign. For example:\n\nJohn 11:35 = Jesus Wept."
             #need to check if front part of text is a book in the Bible, if it is, then it will be a custom verse
             #split string on equals at this point
             #put a new verse in user's verses
